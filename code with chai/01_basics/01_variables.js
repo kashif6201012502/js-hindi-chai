c1df2baff08ecad7ca36eb45da289e2d2fb29a06
MD KASHIF ANSARI <62875550+kashif6201012502@users.noreply.github.com>
2025-08-16 00:49:19 +0530

    Add files via upload

---
+++ code with chai/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 11121
+
+let accountEmail  = "kashif6201012502@gmail.com";
+
+var accountPassword = "kasif123"
+
+accountCity = "jharkhand";
---
+++ code with chai/01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+//let score = 33
+
+//console.log(typeof score)
+
+
+//"33"  => 33
+
+//"33abc" => Nan
+
+// true = 1; false = 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"kasjif" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber)
+
+console.log(typeof stringNumber);
+
